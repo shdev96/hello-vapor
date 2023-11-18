b90436ec33a2acf2f3376a0109c0679c362957e1
이승희 <iseunghui@iseunghuiui-MacBookPro.local>
2023-11-18 18:04:01 +0900

    init

---
+++ Sources/App/routes.swift
@@ -0,0 +1,28 @@
+import Vapor
+
+func routes(_ app: Application) throws {
+    app.get { req async in
+        "It works!"
+    }
+
+    app.get("hello") { req async -> String in
+        "Hello, world!"
+    }
+    
+    app.get("movies", ":genre") { req async throws -> String in
+        
+        guard let genre = req.parameters.get("genre") else {
+            throw Abort(.badRequest)
+        }
+        return "All movies of genre: \(genre)"
+    }
+    
+    app.get("customers", ":customerId") {req async throws -> String in
+        
+        guard let customerId = req.parameters.get("customerId", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
+        return "\(customerId)"
+    }
+    
+}
